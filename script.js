0953fe4805a2f2b2722d4ccdfe3c600d8203d735
Mitali Shinde <85117547+mitalishinde08@users.noreply.github.com>
2022-09-14 01:18:38 +0530

    Added task delete, task completed functionalities

--- script.js
+++ script.js
@@ -4,18 +4,34 @@ let task_box = document.getElementById('main-input');
 let add = document.getElementById('add');
 
 add.addEventListener('click',add_task);
-let counter=0;
 
 function add_task() {
-    counter++;
     var el1 = document.createElement("div");
     el1.innerHTML = `<input type="text">
     <img src="/assets/tickmark.svg" class="tick">
-    <img src="/assets/notebook-pen-icon.svg" class="note">
     <img src="/assets/delete.svg" class="del">`;
     el1.classList.add("box_task");
     document.body.appendChild(el1);
     el1.querySelector('input').value = task_box.value;
+    task_box.value=''
+
+    // delete tasks
+    let del_task_btn = el1.querySelector('.del');
+
+    del_task_btn.addEventListener('click',delete_task)
+
+    function delete_task() {
+        del_task_btn.parentElement.remove()
+    }
+
+    // complete tasks
+    let done_task_btn = el1.querySelector('.tick');
+
+    done_task_btn.addEventListener('click',done_task)
+
+    function done_task() {
+        el1.querySelector('input').style.textDecoration='line-through';
+    }
 }
 
 let del = document.querySelector('.del');
@@ -25,6 +41,13 @@ del.addEventListener('click',clear_task);
 function clear_task() {
     task_box.value = '';
 }
+
+
+
+
+
+
+
 
 
 
